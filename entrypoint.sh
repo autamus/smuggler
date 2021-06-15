ae06981f66fb8a2dcc8f57c02385b332cfa06a29
Alec Scott <alec@bcs.sh>
2021-06-15 15:48:02 -0700

    Add Smuggler Workflow

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sh -c "docker run -d \
+                  -p $INPUT_GENERAL_PORT:$INPUT_GENERAL_PORT \
+                  -e INPUT_GENERAL_PORT=$INPUT_GENERAL_PORT \
+                  -e INPUT_SAVE_PATH=$INPUT_SAVE_PATH \
+                  -v $INPUT_SAVE_PATH:$INPUT_SAVE_PATH \
+                  ghcr.io/autamus/smuggler:latest"
