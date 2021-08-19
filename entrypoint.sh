0170fca03c3d0cae9e9f2d9625fdbcece58dbc90
Alec Scott <alecbcs@gmail.com>
2021-08-19 12:41:40 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,5 @@ sh -c "docker run -d \
                   -e INPUT_GENERAL_PORT=$INPUT_GENERAL_PORT \
                   -e INPUT_SAVE_PATH=$INPUT_SAVE_PATH \
                   -v $INPUT_SAVE_PATH:$INPUT_SAVE_PATH \
+                  --restart always \
                   ghcr.io/autamus/smuggler:latest"
