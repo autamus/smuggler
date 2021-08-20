c6a054739a49d73a91d55abbe7a0bd6ea2e67b0b
Alec Scott <alec@bcs.sh>
2021-08-20 13:53:19 -0700

    Only expose locally

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sh -c "docker run -d \
-                  -p $INPUT_GENERAL_PORT:$INPUT_GENERAL_PORT \
+                  -p 127.0.0.1:$INPUT_GENERAL_PORT:$INPUT_GENERAL_PORT \
                   -e INPUT_GENERAL_PORT=$INPUT_GENERAL_PORT \
                   -e INPUT_SAVE_PATH=$INPUT_SAVE_PATH \
                   -v $INPUT_SAVE_PATH:$INPUT_SAVE_PATH \
